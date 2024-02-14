68e1ce1426e34aa5fe436809f82ce7da77794df4
Lucien <lucien.neuhoff@protonmail.com>
2024-02-14 18:55:10 +0100

    fix: shadcn alias

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			$shadcn: './src/lib/sahdcn/components/ui',
+			$shadcn: './src/lib/shadcn/components/ui',
 			$cn: './src/lib/shadcn/utils',
 			$shared: './src/shared'
 		}
