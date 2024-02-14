f9a239736c125e31195cf645bd263e9f29537b62
Lucien <lucien.neuhoff@protonmail.com>
2024-02-14 19:18:53 +0100

    feat: moved navbar

---
+++ src/lib/components/ui/nav/navbar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import urls from '$lib/urls';
+	import { Button } from '$shadcn/button';
+</script>
+
+<nav class="flex gap-3 p-3">
+	<ul>
+		<li><Button href={urls.auth.signin}>Sign In</Button></li>
+	</ul>
+	<ul>
+		<li><Button href={urls.auth.signup}>Sign Up</Button></li>
+	</ul>
+	<ul>
+		<li><Button href={urls.auth.signout}>Sign Out</Button></li>
+	</ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
+	import Navbar from '$lib/components/ui/nav/navbar.svelte';
 	import '../app.pcss';
 </script>
+
+<Navbar />
 
 <slot />
