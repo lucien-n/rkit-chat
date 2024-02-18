80adaaea3cc2d509e5f9e0d53018b532f160569b
Lucien <lucien.neuhoff@protonmail.com>
2024-02-18 18:58:47 +0100

    ref: removed console log of messages

--- src/hooks/handleRemult.ts
+++ src/hooks/handleRemult.ts
@@ -3,14 +3,12 @@ import { createPostgresDataProvider } from 'remult/postgres';
 import { DATABASE_URL } from '$env/static/private';
 import { controllers, entities } from '$shared';
 import { AuthUser } from '$shared/modules/auth/auth_user.entity';
-import { Message } from '$shared/modules/messages/message.entity';
 import { ProfilesController } from '$shared/modules/profiles/profiles.controller';
 
 export const handleRemult = remultSveltekit({
 	dataProvider: createPostgresDataProvider({ connectionString: DATABASE_URL }),
 	initApi: async (remult) => {
 		console.table(await remult.repo(AuthUser).find());
-		console.table(await remult.repo(Message).find());
 	},
 	getUser: async (event) => {
 		if (!event.locals.user) return undefined;
