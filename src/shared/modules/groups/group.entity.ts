01d1b4b6695e539c80dedb9c55fbaeb23cb7f1cf
Lucien <lucien.neuhoff@protonmail.com>
2024-02-18 21:09:13 +0100

    wip: autorizations

--- src/shared/modules/groups/group.entity.ts
+++ src/shared/modules/groups/group.entity.ts
@@ -1,8 +1,14 @@
-import { Entity, Fields, Relations } from 'remult';
+import { Entity, Fields, Relations, remult } from 'remult';
 import { Profile } from '../profiles/profile.entity';
 import { GroupsToProfiles } from '../groups-to-profile/groups-to-profile.entity';
 
-@Entity<Group>('groups')
+@Entity<Group>('groups', {
+	apiPrefilter: () => {
+		return {
+			$or: [{ adminId: remult.user?.id }, {}] // todo: try and implement something like { profiles: { $contains: remult.user?.id } }
+		};
+	}
+})
 export class Group {
 	@Fields.uuid()
 	id!: string;
