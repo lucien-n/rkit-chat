353e3081c53b469fba76e23c88529ad05e2d7edd
Lucien <lucien.neuhoff@protonmail.com>
2024-02-19 19:21:54 +0100

    remult debug include

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,9 +1,9 @@
 import { AuthController } from './modules/auth/auth.controller';
 import { AuthSession } from './modules/auth/auth_session.entity';
 import { AuthUser } from './modules/auth/auth_user.entity';
-import { GroupsToProfileController } from './modules/groups-to-profiles/groups-to-profiles.controller';
 import { GroupsToProfiles } from './modules/groups-to-profiles/groups-to-profiles.entity';
 import { Group } from './modules/groups/group.entity';
+import { GroupsController } from './modules/groups/groups.controller';
 import { Message } from './modules/messages/message.entity';
 import { MessagesController } from './modules/messages/messages.controller';
 import { Profile } from './modules/profiles/profile.entity';
@@ -14,5 +14,5 @@ export const controllers = [
 	AuthController,
 	MessagesController,
 	ProfilesController,
-	GroupsToProfileController
+	GroupsController
 ];
