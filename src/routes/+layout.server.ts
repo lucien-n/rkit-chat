8e833c4daacacbc638a0841ba8a53d1954be9b3c
Lucien <lucien.neuhoff@protonmail.com>
2024-02-24 18:22:15 +0100

    ref

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,9 +3,7 @@ import type { LayoutServerLoad } from './$types';
 import { UsersController } from '$shared/modules/users/users.controller';
 
 export const load: LayoutServerLoad = async ({ locals: { authSession: session } }) => {
-	const user = remult.user
-		? await UsersController.findById(remult.user?.id, { settings: true })
-		: null;
+	const user = remult.user ? await UsersController.findById(remult.user?.id) : null;
 
 	return {
 		session,
