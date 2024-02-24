1a7522ade55e9630d132baf5eb878ac15837475a
Lucien <lucien.neuhoff@protonmail.com>
2024-02-24 18:52:18 +0100

    ref: renamed group avatar to group mini

--- src/lib/components/ui/chat/toolbar.svelte
+++ src/lib/components/ui/chat/toolbar.svelte
@@ -3,7 +3,7 @@
 	import type { Group } from '$shared/modules/groups/group.entity';
 	import type { CreateGroupSchema } from '$shared/modules/groups/schemas/create-group.schema';
 	import CreateGroupDialog from '$ui/group/create-group-dialog.svelte';
-	import GroupAvatar from '$ui/group/group-avatar.svelte';
+	import GroupMini from '$ui/group/group-mini.svelte';
 	import UserMini from '$ui/user/user-mini.svelte';
 	import type { SuperValidated } from 'sveltekit-superforms';
 
@@ -17,7 +17,7 @@
 	<div class="flex h-full flex-col justify-between">
 		<div class="flex flex-col gap-3">
 			{#each groups as group}
-				<GroupAvatar {group} />
+				<GroupMini {group} />
 			{/each}
 		</div>
 		<!-- TODO: create group dialog directly using remult and not sveltekit forms OR find a cleaner way to use forms (endpoints) -->
