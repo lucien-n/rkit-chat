3861ba5be1110e5416b0bc69b2b687d49e54580e
Lucien <lucien.neuhoff@protonmail.com>
2024-02-25 18:17:25 +0100

    updated group header style

--- src/lib/components/ui/group/group-chat-header.svelte
+++ src/lib/components/ui/group/group-chat-header.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import { getGroup } from '$contexts/group';
-	import { userStore } from '$stores/stores';
 	import { Badge } from '$shadcn/badge';
+	import Separator from '$shadcn/separator/separator.svelte';
+	import { userStore } from '$stores/stores';
 	import { H3, Muted } from '$typography';
 
 	let group = getGroup();
 </script>
 
 {#if $group}
-	<div class="w-full self-start rounded-t-md border-b px-4 py-2">
+	<div class="w-full self-start rounded-t-md px-4 py-2">
 		<div class="flex items-center gap-2">
 			<H3>{$group.name}</H3>
 			{#if $group.adminId === $userStore?.id}
@@ -16,5 +17,6 @@
 			{/if}
 		</div>
 		<Muted>{$group.userCount} user(s)</Muted>
+		<Separator class="mt-2" />
 	</div>
 {/if}
