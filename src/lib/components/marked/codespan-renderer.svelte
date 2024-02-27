bed2fd5d78c018cc5f986c048c6428b0cb232229
Lucien <lucien.neuhoff@protonmail.com>
2024-02-27 21:34:51 +0100

    wip: codepsan copy to clipboard

--- src/lib/components/marked/codespan-renderer.svelte
+++ src/lib/components/marked/codespan-renderer.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
+	import { Button } from '$shadcn/button';
+	import { ClipboardCopy } from 'radix-icons-svelte';
+
 	export let text: string;
+
+	const handleCopy = () => {};
 </script>
 
-<p class="inline-block w-fit rounded-lg border px-2 py-1 font-mono">{text}</p>
+<div class="inline-block w-fit rounded-lg border">
+	<p class="inline-block py-1 pl-2 font-mono">
+		{text}
+	</p>
+	<Button
+		variant="ghost"
+		class="mr-1 inline-block h-6 w-6 self-center p-1 align-middle"
+		on:click={handleCopy}
+	>
+		<ClipboardCopy />
+	</Button>
+</div>
