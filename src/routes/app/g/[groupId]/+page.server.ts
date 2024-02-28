ffbc4bbffe21d7e72ad18e7c94d09409ed8757a8
Lucien <lucien.neuhoff@protonmail.com>
2024-02-28 16:53:26 +0100

    fix: redirect user if not in group or if no group found

--- src/routes/app/g/[groupId]/+page.server.ts
+++ src/routes/app/g/[groupId]/+page.server.ts
@@ -1,16 +1,20 @@
-import type { RemultError } from '$shared/helpers/types';
-import { GroupsController } from '$shared/modules/groups/groups.controller';
-import { createGroupSchema } from '$shared/modules/groups/schemas/create-group.schema';
-import { MessagesController } from '$shared/modules/messages/messages.controller';
-import { createMessageSchema } from '$shared/modules/messages/schemas/create-message.schema';
-import { fail } from '@sveltejs/kit';
+import { remult } from 'remult';
+import { urls } from '$lib/urls';
+import { fail, redirect } from '@sveltejs/kit';
 import { zod } from 'sveltekit-superforms/adapters';
-import { message, superValidate } from 'sveltekit-superforms/server';
 import type { Actions, PageServerLoad } from './$types';
+import type { RemultError } from '$shared/helpers/types';
+import { message, superValidate } from 'sveltekit-superforms/server';
+import { GroupsController } from '$shared/modules/groups/groups.controller';
+import { MessagesController } from '$shared/modules/messages/messages.controller';
+import { createGroupSchema } from '$shared/modules/groups/schemas/create-group.schema';
+import { createMessageSchema } from '$shared/modules/messages/schemas/create-message.schema';
 
 export const load: PageServerLoad = async (event) => {
 	const { groupId } = event.params;
 	const group = (await GroupsController.findById(groupId)) ?? null;
+
+	if (!group || !remult.user?.groups.includes(group?.id)) redirect(307, urls.home.root);
 
 	return {
 		messageForm: await superValidate(zod(createMessageSchema)),
