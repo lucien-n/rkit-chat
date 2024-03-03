e12be8aa6c87950c0e0dfba63284975245faecb9
Lucien <lucien.neuhoff@protonmail.com>
2024-03-03 16:21:03 +0100

    ref: removed initApi logging from handleRemult

--- src/lib/server/remult.ts
+++ src/lib/server/remult.ts
@@ -1,14 +1,9 @@
-import type { Remult, UserInfo } from 'remult';
+import type { UserInfo } from 'remult';
 import type { RequestEvent } from '@sveltejs/kit';
-import { Group } from '$shared/modules/groups/group.entity';
-import { AuthUser } from '$shared/modules/auth/auth_user.entity';
 import { UsersController } from '$shared/modules/users/users.controller';
 import { GroupsController } from '$shared/modules/groups/groups.controller';
 
-export const initApi = async (remult: Remult) => {
-	console.table(await remult.repo(AuthUser).find());
-	console.table(await remult.repo(Group).find({ include: { users: true, admin: true } }));
-};
+export const initApi = async () => {};
 
 export const getUser = async (event: RequestEvent): Promise<UserInfo | undefined> => {
 	if (!event.locals.authUser) return undefined;
