6435d6d262370928299ea8557562251837c6c57f
Lucien <lucien.neuhoff@protonmail.com>
2024-03-03 17:51:40 +0100

    cleanup: removed createGroup action in u route

--- src/routes/app/u/[handle]/+page.server.ts
+++ src/routes/app/u/[handle]/+page.server.ts
@@ -1,13 +1,9 @@
 import { rejson } from '$helpers/rejson';
-import { error, fail } from '@sveltejs/kit';
-import { zod } from 'sveltekit-superforms/adapters';
-import type { Actions, PageServerLoad } from './$types';
-import type { User } from '$shared/modules/users/user.entity';
-import { message, superValidate } from 'sveltekit-superforms/server';
-import { UsersController } from '$shared/modules/users/users.controller';
-import { GroupsController } from '$shared/modules/groups/groups.controller';
 import { FriendsController } from '$shared/modules/friends/friends.controller';
-import { createGroupSchema } from '$shared/modules/groups/schemas/create-group.schema';
+import type { User } from '$shared/modules/users/user.entity';
+import { UsersController } from '$shared/modules/users/users.controller';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const { handle } = params;
@@ -41,20 +37,4 @@ export const load: PageServerLoad = async ({ params }) => {
 		sentFriendRequests: await Promise.all(sentFriendRequests),
 		friends
 	};
-};
-
-export const actions: Actions = {
-	createGroup: async (event) => {
-		const form = await superValidate(event, zod(createGroupSchema));
-		if (!form.valid) {
-			return fail(400, {
-				form
-			});
-		}
-		const { name } = form.data;
-
-		await GroupsController.create({ name });
-
-		return message(form, 'Group created');
-	}
 };
