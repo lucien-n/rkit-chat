1c7e65b1bd4dedfccb39387ba7a8dd7ec6da851d
Lucien <lucien.neuhoff@protonmail.com>
2024-03-03 16:13:58 +0100

    fix: cuid param matcher

--- src/params/cuid.ts
+++ src/params/cuid.ts
@@ -1,3 +1,3 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 
-export const match: ParamMatcher = (param) => /^[a-z0-9]{7,}$/.test(param);
+export const match: ParamMatcher = (param) => /^[a-zA-Z0-9]{7,}$/.test(param);
